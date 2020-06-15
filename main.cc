fd0f03ef8d13934ed95784f52a98514ea44e3c82
ShakaUVM </u/ShakaUVM>
2020-06-15 00:07:44 -0700

    Added boilerplate code

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+int main() {
+	//Write code here to print "Hello World!" to the screen followed by a newline
+}
